b93ad80419342812438eb6f92bc48c48e88db06f
Philo-Sophia <xingfanxia@gmail.com>
2024-09-18 04:22:17 -0700

    init

---
+++ src/lib/slices/Hero/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Content } from '@prismicio/client';
+
+  export let slice: Content.HeroSlice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+  <h1 class="mb-2 md-8 text-[clamp(3rem,20vmin,13rem)] font-extrabold leading-none tracking-tighter text-nowrap">
+    <span>
+      {slice.primary.first_name}
+    </span>
+    <span>
+      {slice.primary.last_name}
+    </span>
+  </h1>
+  
+  <span>
+    {slice.primary.tag_line}
+  </span>
+</section>
